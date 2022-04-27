b8b926fda842902f699963556e69047caed56990
Pels <pelumisunday@gmail.com>
2022-04-27 04:23:30 +0100

    feat: create and include layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.scss';
+import Footer from './components/Footer';
+import Graph from './components/Graph';
+import Header from './components/Header';
+import Info from './components/Info';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Header />
+      <Info />
+      <Graph />
+      <Footer />
     </div>
   );
 }
