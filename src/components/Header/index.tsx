e518d9c9db3270a9891d73875883c8c49c68b804
Pels <pelumisunday@gmail.com>
2022-04-28 06:54:12 +0100

    feat: create mobile nav and active states

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,67 @@
+import { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import logoWhite from '../../assets/logo-white.svg'
+import logoRed from '../../assets/logo-red.svg'
+import logoFull from '../../assets/logo-full.svg'
+import hamburger from '../../assets/hamburger.svg'
+import close from '../../assets/close.svg'
+import exit from '../../assets/exit.svg'
+import home from '../../assets/home.svg'
+import user from '../../assets/user.svg'
+import settings from '../../assets/settings.svg'
 import './index.scss'
-import logo from '../../assets/logo.svg'
-import toggle from '../../assets/hamburger.svg'
 
 const Header = () => {
+  const navRef = useRef<HTMLDivElement>(null)
+  const [isActive, setIsActive] = useState(false)
+
+  const handleClick = () => {
+    console.log(navRef.current?.classList.toggle('active'))
+    setIsActive(!isActive)
+  }
+
   return (
-    <nav className='nav'>
-      <div className='nav__left'>
-        <div className='nav__logo'>
-          <img src={logo} alt='logo' />
+    <div className='wrapper' ref={navRef}>
+      <nav className='nav'>
+        <div className='nav__left'>
+          <div className='nav__logo'>
+            <img src={isActive ? logoRed : logoWhite} alt='logo' />
+          </div>
+          <p className='nav__text'>
+            Hallo, <span>LIQID!</span>
+          </p>
         </div>
-        <p className='nav__text'>
-          Hallo, <span>LIQID!</span>
-        </p>
-      </div>
-      <div className='nav__right'>
-        <img className='nav__toggle' src={toggle} alt='' />
-      </div>
-    </nav>
+        <div className='nav__right'>
+          <img
+            className='nav__toggle'
+            src={isActive ? close : hamburger}
+            alt=''
+            onClick={handleClick}
+          />
+        </div>
+      </nav>
+
+      <nav className='subNav'>
+        <Link to='/' className='subNav__link'>
+          <img src={exit} alt='logout icon' />
+          <span>Logout</span>
+        </Link>
+        <Link to='/' className='subNav__link'>
+          <img src={home} alt='home icon' />
+          <span>Home</span>
+        </Link>
+        <Link to='/profile' className='subNav__link'>
+          <img src={user} alt='profile icon' />
+          <span>Profile</span>
+        </Link>
+        <Link to='/settings' className='subNav__link'>
+          <img src={settings} alt='settings icon' />
+          <span>Settings</span>
+        </Link>
+      </nav>
+
+      <img className='menu-image' src={logoFull} alt='logo' />
+    </div>
   )
 }
 
