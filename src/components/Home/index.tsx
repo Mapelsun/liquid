93753cfb88285311f5f0f8be547a235f84baea5d
Pels <pelumisunday@gmail.com>
2022-04-28 06:54:33 +0100

    feat: add home component

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,15 @@
+import Graph from '../Graph'
+import Info from '../Info'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <>
+      <Info />
+      <Graph />
+    </>
+  )
+}
+
+export default Home
+
