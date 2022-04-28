b8f58c3bb2dfae62eaeb042cd3ed0b0eb203dec3
Pels <pelumisunday@gmail.com>
2022-04-28 06:55:09 +0100

    feat: create additional route components

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,7 @@
+import './index.scss'
+
+const Profile = () => {
+  return <div>Profile</div>
+}
+
+export default Profile
---
+++ src/components/Settings/index.tsx
@@ -0,0 +1,9 @@
+import './index.scss'
+
+const Settings = () => {
+  return (
+    <div>Settings</div>
+  )
+}
+
+export default Settings
