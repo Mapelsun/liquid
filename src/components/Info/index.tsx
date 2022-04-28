af5c90255b2ce8e877ee5a8f3f472ffe1b93ccc1
Pels <pelumisunday@gmail.com>
2022-04-28 16:40:47 +0100

    feat: create responsive hero info section

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -1,9 +1,46 @@
+import arrowRight from '../../assets/arrow-right.svg'
 import './index.scss'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <section className='info'>
+      <div className='info__left'>
+        <h2>Welcome to your personal area.</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          fermentum elit id auctor mollis. Aenean feugiat commodo quam,
+          vulputate viverra lorem iaculis in. Curabitur varius commodo lacus
+          eget vestibulum. Curabitur vitae risus nec justo faucibus vulputate.
+          Nunc blandit nisi lobortis, gravida elit non, tincidunt dolor. Mauris
+          ullamcorper suscipit risus, eget dictum dui consequat sed. Nam quis
+          orci id dui consectetur elementum. Nulla et lacus massa.
+        </p>
+      </div>
+      <div className='info__right'>
+        <div className='info__box'>
+          <h3>Investments</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            fermentum elit id auctor mollis. Aenean feugiat commodo quam,
+            vulputate viverra lorem iaculis in. Curabitur varius commodo lacus
+            eget vestibulum. Curabitur vitae risus nec justo faucibus vulputate.
+          </p>
+        </div>
+        <div className='info__box'>
+          <h3>Stocks</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            fermentum elit id auctor mollis.
+          </p>
+          <button>
+            <span>Explore</span>
+            <img src={arrowRight} alt='' />
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
 export default Info
+
