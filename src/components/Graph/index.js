97fa204aea7bc87c6e878ec969110e0d72dce8ed
Pels <pelumisunday@gmail.com>
2022-04-30 20:15:28 +0100

    feat: build bar graph with data

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -2,86 +2,117 @@ import { useD3 } from '../../hooks/useD3'
 import * as d3 from 'd3'
 import './index.scss'
 
-const dataMain = [
-  { product: 'LIQID Cash', initial: 92, total: 920 },
-  { product: 'LIQID Real Estate', initial: 63, total: 630 },
-  { product: 'LIQID Wealth', initial: 85, total: 850 },
-  { product: 'LIQID Private Equity', initial: 22, total: 220 },
-  { product: 'LIQID Venture', initial: 51, total: 510 },
-]
-
-console.log(dataMain)
-
 const Graph = () => {
   const data = [
-    { year: 1980, efficiency: 24.3, sales: 8949000 },
-    { year: 1985, efficiency: 27.6, sales: 10979000 },
-    { year: 1990, efficiency: 28, sales: 9303000 },
-    { year: 1991, efficiency: 28.4, sales: 8185000 },
-    { year: 1992, efficiency: 27.9, sales: 8213000 },
-    { year: 1993, efficiency: 28.4, sales: 8518000 },
-    { year: 1994, efficiency: 28.3, sales: 8991000 },
+    { name: 'LIQID Cash', initial: 92, value: 920, color: '#C9B7C5' },
+    { name: 'LIQID Real Estate', initial: 63, value: 630, color: '#AFDDAF' },
+    { name: 'LIQID Wealth', initial: 85, value: 850, color: '#076283' },
+    {
+      name: 'LIQID Private Equity',
+      initial: 22,
+      value: 220,
+      color: '#79C6C0',
+    },
+    { name: 'LIQID Venture', initial: 51, value: 510, color: '#FFE163' },
   ]
+
+  // const data = [
+  //   { name: 'apple', price: 100 },
+  //   { name: 'limon', price: 300 },
+  //   { name: 'strawberry', price: 150 },
+  //   { name: 'blueberry', price: 250 },
+  // ]
 
   const ref = useD3(
     (svg) => {
-      const height = 500
-      const width = 500
-      const margin = { top: 20, right: 30, bottom: 30, left: 40 }
+      // Responsive Graph
+
+      // const svgHeight = svg.node().getBoundingClientRect().width
+      // const svgWidth = svg.node().getBoundingClientRect().height
+
+      const svgHeight = 600
+      const svgWidth = 600
+      const graphHeight = 500
+      const graphWidth = 500
+      const graphMarginLeft = (svgHeight - graphHeight) / 2
+      const graphMarginTop = (svgWidth - graphWidth) / 2
+      const maxValue = d3.max(data, (d) => d.value)
 
       const x = d3
         .scaleBand()
-        .domain(data.map((d) => d.year))
-        .rangeRound([margin.left, width - margin.right])
-        .padding(0.1)
+        .domain(data.map((it) => it.name))
+        .range([0, graphWidth])
+        .paddingInner(0.2)
+        .paddingOuter(0.2)
 
-      const y1 = d3
-        .scaleLinear()
-        .domain([0, d3.max(data, (d) => d.sales)])
-        .rangeRound([height - margin.bottom, margin.top])
+      const y = d3.scaleLinear().domain([0, maxValue]).range([graphHeight, 0])
 
-      const xAxis = (g) =>
-        g.attr('transform', `translate(0,${height - margin.bottom})`).call(
-          d3
-            .axisBottom(x)
-            .tickValues(
-              d3
-                .ticks(...d3.extent(x.domain()), width / 40)
-                .filter((v) => x(v) !== undefined)
-            )
-            .tickSizeOuter(0)
-        )
+      const xAxis = d3.axisBottom(x)
+      const yAxis = d3.axisLeft(y)
 
-      const y1Axis = (g) =>
-        g
-          .attr('transform', `translate(${margin.left},0)`)
-          .style('color', 'steelblue')
-          .call(d3.axisLeft(y1).ticks(null, 's'))
-          .call((g) => g.select('.domain').remove())
-          .call((g) =>
-            g
-              .append('text')
-              .attr('x', -margin.left)
-              .attr('y', 10)
-              .attr('fill', 'currentColor')
-              .attr('text-anchor', 'start')
-              .text(data.y1)
-          )
-
-      svg.select('.x-axis').call(xAxis)
-      svg.select('.y-axis').call(y1Axis)
+      const tooltip = d3
+        .select('body')
+        .append('div')
+        .style('position', 'absolute')
+        .style('z-index', '10')
+        .style('visibility', 'hidden')
+        .style('padding', '10px')
+        .style('background', 'rgba(255,255,255,1)')
+        .style('border-radius', '0 0 0 1.2rem')
+        .style('color', 'rgba(0,0,0,1)')
 
       svg
-        .select('.plot-area')
-        .attr('fill', 'steelblue')
-        .selectAll('.bar')
+        .attr('viewBox', [0, 0, svgWidth, svgHeight])
+        .attr('height', svgHeight)
+        .attr('width', svgWidth)
+
+      const bar = svg
+        .append('g')
+        .attr('height', graphHeight)
+        .attr('width', graphWidth)
+        .attr(
+          'transform',
+          `translate(${(svgWidth - graphWidth) / 2}, ${
+            (svgHeight - graphHeight) / 2
+          })`
+        )
+
+      bar
+        .selectAll('rect')
         .data(data)
         .join('rect')
-        .attr('class', 'bar')
-        .attr('x', (d) => x(d.year))
-        .attr('width', x.bandwidth())
-        .attr('y', (d) => y1(d.sales))
-        .attr('height', (d) => y1(0) - y1(d.sales))
+        .attr('width', x.bandwidth)
+        .attr('height', (d) => graphHeight - y(d.value))
+        .attr('x', (d) => x(d.name))
+        .attr('y', (d) => y(d.value))
+        .attr('fill', (d) => d.color)
+        .on('mouseover', function (e, d, i) {
+          tooltip.html(`<div>${d.name}</div>`).style('visibility', 'visible')
+
+          d3.select(this).attr('fill', 'var(--clr-red)')
+        })
+        .on('mousemove', function (e) {
+          tooltip
+            .style('top', e.pageY + 10 + 'px')
+            .style('left', e.pageX + 10 + 'px')
+        })
+        .on('mouseout', function () {
+          tooltip.html(``).style('visibility', 'hidden')
+          d3.select(this).transition().attr('fill', (d) => d.color)
+        })
+
+      svg
+        .append('g')
+        .attr('transform', `translate(${graphMarginLeft}, ${graphMarginTop})`)
+        .call(yAxis)
+
+      svg
+        .append('g')
+        .attr(
+          'transform',
+          `translate(${graphMarginLeft}, ${svgHeight - graphMarginTop})`
+        )
+        .call(xAxis)
     },
     [data.length]
   )
@@ -89,12 +120,7 @@ const Graph = () => {
   return (
     <section className='graph'>
       <h2 className='graph__heading'>Your products</h2>
-      <svg
-        ref={ref}
-        className="graph__svg">
-        <g className='plot-area' />
-        <g className='x-axis' />
-        <g className='y-axis' />
+      <svg ref={ref} className='graph__svg'>
       </svg>
     </section>
   )
